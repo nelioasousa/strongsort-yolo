c1658c55e9be92dae83b2ed1e4996149051f97d3
nelioasousa <corneliosousa@alu.ufc.br>
2023-09-03 13:30:41 -0300

    torchreid dependency as a package

--- strong_sort/strong_sort.py
+++ strong_sort/strong_sort.py
@@ -7,9 +7,7 @@ import os.path as osp
 from .sort.nn_matching import NearestNeighborDistanceMetric
 from .sort.detection import Detection
 from .sort.tracker import Tracker
-from .deep.reid_model_factory import show_downloadeable_models, get_model_url, get_model_name
 
-sys.path.append(osp.join(osp.dirname(__file__), 'deep', 'reid'))
 from torchreid.utils import FeatureExtractor
 from torchreid.utils.tools import download_url
 
